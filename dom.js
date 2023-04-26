2cb9f3195b3c33b6e211710f4899d2b22697c745
Gunjannegi <gunjan.negi.5249@gmail.com>
2023-04-26 21:00:38 +0530

    third commit

--- dom.js
+++ dom.js
@@ -10,3 +10,8 @@
 var Additems=document.getElementsByClassName('title');
 Additems[0].style.color='green';
 Additems[0].style.fontWeight='bold';
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold'
+}
