adc5592c9443658b84782a83cbf4cabc23fc584b
Gunjannegi <gunjan.negi.5249@gmail.com>
2023-04-26 17:52:08 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,12 @@
+//GETELEMENTBYID
+//var headerTitle=document.getElementById('header-title');
+//var header=document.getElementById('main-header');
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Goodbye';
+//headerTitle.innerHTML='<h3>Hello</h3>'
+//header.style.borderBottom='solid 7px #000';
+
+//GETELMENTBYCLASSNAME//
+var Additems=document.getElementsByClassName('title');
+Additems[0].style.color='green';
+Additems[0].style.fontWeight='bold';
